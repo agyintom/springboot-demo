aa6edfa99ea8a2d43805458969dd9d98edfae230
agyintom <tom.agyin@outlook.com>
2026-05-16 19:45:56 +0200

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -15,6 +15,6 @@ public class DemoApplication {
 
     @GetMapping("/")
     public String hello() {
-        return "Hey there! Welcome to my Springboot demo application. built by AJ Tom on 2026-05-06 Lets have fun";
+        return "Hey there! Welcome to my Springboot demo application. built by AJ Tom on 2026-05-06 with argocd comparing live state to the the desire state";
     }
 }
